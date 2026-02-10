e17cc76e779c10e16f7d4d061be21cbddf0f28ca
navaneetsharma22 <navaneetsharma22@gmail.com>
2026-02-10 20:29:23 +0530

    second commit

--- routes/routesTodo.js
+++ routes/routesTodo.js
@@ -3,8 +3,12 @@ const router = express.Router();   // ✅ FIXED
 
 // import controller
 const { createTodo } = require("../controllers/createTodo");
+const {getTodo} = require("../controllers/createTodo");
+
 
 // define API routes
 router.post("/createTodo", createTodo);
+router.get("/getTodos",getTodo);
+
 
 module.exports = router;
